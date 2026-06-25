de086b9fc98d04797344f389d6b12318a66c8051
Michel Mendes <michel.gmsm@gmail.com>
2026-06-25 00:37:26 -0300

    feat(lote): implementa modelo e tipos para Lote
    - Este modelo representa o lote específico de qualquer produtos categorizado como  piso/porcelanato

---
+++ src/modules/lote/lote.model.ts
@@ -0,0 +1,31 @@
+import { Schema, model, models, Model } from 'mongoose';
+import { ILote } from './lote.types';
+
+const LoteSchema = new Schema<ILote>(
+    {
+        ativo: { type: Boolean, default: true },
+
+        idProduto: { type: Schema.Types.ObjectId, ref: 'Produto', required: true, index: true },
+        idDeposito: { type: Schema.Types.ObjectId, ref: 'Deposito', required: true, index: true },
+
+        dataProducao: { type: Date },
+        codigoLote: { type: String, required: true, trim: true, uppercase: true },
+        bitola: { type: Number, required: true },
+        tonalidade: { type: Number, required: true },
+
+        quantidadeInicial: { type: Number, required: true, min: 0 },
+        quantidadeAtual: { type: Number, required: true, min: 0 },
+        quantidadeReservada: { type: Number, default: 0, min: 0 },
+
+        localizacaoDetalhada: { type: String, trim: true, uppercase: true },
+        observacoes: { type: String, trim: true },
+    },
+    { timestamps: true }
+);
+
+// Índices
+LoteSchema.index({ idProduto: 1, ativo: 1 });       // Busca rápida por produto e lotes ativos (A query mais comum no dia a dia)
+LoteSchema.index({ idDeposito: 1 });                // Busca rápida por depósito
+LoteSchema.index({ idProduto: 1, codigoLote: 1 });  // Busca exata de um lote de um produto
+
+export const Lote: Model<ILote> = models.Lote || model<ILote>('Lote', LoteSchema);
---
+++ src/modules/lote/lote.types.ts
@@ -0,0 +1,26 @@
+import { Types } from "mongoose";
+
+export interface ILote {
+    ativo: boolean;                 // Status do lote (é inativado quando zerado em estoque)
+    
+    idProduto: Types.ObjectId;
+    idDeposito: Types.ObjectId;
+
+    // Especificação do lote
+    dataProducao: Date;
+    codigoLote: string;
+    bitola: number;
+    tonalidade: number;
+
+    // Controle de estoque
+    quantidadeInicial: number;      // Quantidade de entrada quando o lote foi cadastrado
+    quantidadeAtual: number;        // Quantidade em estoque físico
+    quantidadeReservada: number;    // Quantidade reservada para entrega
+
+    // Dados adicionais
+    localizacaoDetalhada: string;   // Complemento para localização, ex: "Corredor 3", "Mesanino 2"...
+    observacoes: string;            // Utilizado para anotações extras, ex: "Caixa rasgada", "Ponta de estoque"...
+
+    createdAt: Date;
+    updatedAt: Date;
+}
