dece6d926810b9faba465f203a10d75b4dce4316
Michel Mendes <michel.gmsm@gmail.com>
2026-08-04 23:03:22 -0300

    feat(lote): implement criarLote method in service layer

--- src/modules/lote/application/lote.service.ts
+++ src/modules/lote/application/lote.service.ts
@@ -3,10 +3,41 @@ import { ILoteRepository, ListarLotesParams } from "../domain/lote.repository";
 import { MongoLoteRepository } from "../infrastructure/lote.mongo.repository";
 import { LotePresenter } from "./lote.presenter";
 import { LoteRespostaApiDTO } from "./lote.dto";
+import z from "zod";
+import { criarLoteSchemaValidacao } from "./lote.validator";
+import { LoteEntity } from "../domain/lote.entity";
 
 export class LoteService {
     constructor(private readonly loteRepository: ILoteRepository) { }
 
+    async criarLote(dados: z.infer<typeof criarLoteSchemaValidacao>): Promise<LoteRespostaApiDTO> {
+        const novoLoteEntity = new LoteEntity({
+            id: "",
+            idProduto: dados.idProduto,
+            idDeposito: dados.idDeposito,
+            bitola: dados.bitola,
+            tonalidade: dados.tonalidade,
+            quantidadeInicial: dados.quantidadeInicial,
+            quantidadeAtual: dados.quantidadeInicial,
+            quantidadeReservada: 0,
+            numeroLote: dados.numeroLote,
+            ativo: true,
+        });
+
+        const loteCriado = await this.loteRepository.criar({
+            idProduto: novoLoteEntity.idProduto,
+            idDeposito: novoLoteEntity.idDeposito,
+            bitola: novoLoteEntity.bitola,
+            tonalidade: novoLoteEntity.tonalidade,
+            quantidadeInicial: novoLoteEntity.quantidadeInicial,
+            numeroLote: novoLoteEntity.numeroLote,
+            localizacaoDetalhada: novoLoteEntity.localizacaoDetalhada,
+            observacoes: novoLoteEntity.observacoes
+        });
+
+        return LotePresenter.toJSON(loteCriado);
+    }
+    
     /**
      * Lista lotes com paginação e limite de resultados por página.
      */
