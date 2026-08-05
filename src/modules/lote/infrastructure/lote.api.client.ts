8867ca37ee58b6e131ed337919dc95fd5ba24209
Michel Mendes <michel.gmsm@gmail.com>
2026-08-04 23:03:59 -0300

    feat(lote): add criarLoteApi client function

--- src/modules/lote/infrastructure/lote.api.client.ts
+++ src/modules/lote/infrastructure/lote.api.client.ts
@@ -1,10 +1,17 @@
 import { PaginatedResponse } from "@/types/pagination.types";
+import { CriarLoteDTO } from "../application/lote.dto";
 import { LoteRespostaApiDTO } from "../application/lote.dto";
+import { ApiResponse } from "@/types/api-response.types";
 
 interface FetchLoteParams {
     pagina: number;
     limite: number;
 }
+
+const extrairErro = (data: ApiResponse<LoteRespostaApiDTO>, mensagemPadrao: string) => {
+    const msgErro = `${data.error?.message || ""}${(data.error?.details) ? `:\n${data.error.details}` : ""}`
+    return new Error(msgErro || mensagemPadrao);
+};
 
 export async function fetchLotes({limite, pagina}: FetchLoteParams): Promise<PaginatedResponse<LoteRespostaApiDTO>> {
     const url = new URL("/api/lotes", window.location.origin)
@@ -20,4 +27,19 @@ export async function fetchLotes({limite, pagina}: FetchLoteParams): Promise<Pag
 
     const json: PaginatedResponse<LoteRespostaApiDTO> = await response.json()
     return json;
+};
+
+export async function criarLoteApi(dados: CriarLoteDTO): Promise<LoteRespostaApiDTO> {
+    const response = await fetch("/api/lotes", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(dados)
+    });
+
+    const result: ApiResponse<LoteRespostaApiDTO> = await response.json()
+
+    if (!response.ok || !result.success) {
+        throw extrairErro(result, 'Falha ao cadastrar o produto.');
+    }
+    return result.data!;
 }
